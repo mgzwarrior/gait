c34114b9c9f4ac6f15d55f3ad6cfc1bd9a29fcc0
Matt Grant <matt.grant@onepeloton.com>
2023-07-13 23:18:41 -0400

    "\nCommit: Refactor gait.py and test_gait."

--- tests/test_gait.py
+++ tests/test_gait.py
@@ -1,11 +1,14 @@
 import pytest as pytest
+from click.testing import CliRunner
+
+from src.gait import gait
 
 
 class TestGait:
     @pytest.mark.unit
     def test_gait_commit(self):
         # TODO: get CliRunner to work
-        # runner = CliRunner()
-        # result = runner.invoke(gait, ['--verbose', 'commit'])
-        # assert result.exit_code == 0
+        runner = CliRunner()
+        result = runner.invoke(gait, ['commit', '--verbose', '--auto'])
+        assert result.exit_code == 0
         assert 1 == 1
